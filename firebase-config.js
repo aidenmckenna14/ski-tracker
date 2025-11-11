8c4230d2f40cbb5798f89764cd608c2e18739c7d
aidenmckenna14 <aidenmckenna14@gmail.com>
2025-11-11 12:10:50 -0500

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,4 @@
-// Firebase configuration
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Firebase configuration for compat mode
 const firebaseConfig = {
   apiKey: "AIzaSyAtF4Kuf6T-z2Bq1ou1EJTC1vZuzW2KOnU",
   authDomain: "ne-ski-tracker.firebaseapp.com",
@@ -14,9 +8,6 @@ const firebaseConfig = {
   messagingSenderId: "325169000327",
   appId: "1:325169000327:web:4fc6f701633da598646d22"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
